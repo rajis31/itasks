c74931e8f500c395c9bd2ce46322f9ab226e8dd8
Raj <raj350@mail.fresnostate.edu>
2022-07-19 06:55:14 -0700

    setup tailwind in the frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,8 +12,13 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div>
+    <HelloWorld msg="Hello World Component" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div>  
 </template>
 
 <style scoped>
